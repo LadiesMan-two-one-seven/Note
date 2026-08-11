11a837f4a41c9b1bcf97309cc2b810071eed6963
LadiesMan-two-one-seven <asanagaevskiyi@list.ru>
2026-08-11 21:40:36 +0300

    Domain слой. Часть 2

--- app/src/main/java/com/asanagaev/note/domain/SwitchPinnedStatusUseCase.kt
+++ app/src/main/java/com/asanagaev/note/domain/SwitchPinnedStatusUseCase.kt
@@ -1,8 +1,10 @@
 package com.asanagaev.note.domain
 
-class SwitchPinnedStatusUseCase {
+class SwitchPinnedStatusUseCase(
+    private val repository: NotesRepository
+) {
 
     operator fun invoke(noteId: Int) {
-        TODO()
+        repository.switchPinnedStatus(noteId)
     }
 }
