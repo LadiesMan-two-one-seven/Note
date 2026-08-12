9c10567ee950db3b65305ee213ecb93aa6a7214b
LadiesMan-two-one-seven <asanagaevskiyi@list.ru>
2026-08-13 00:29:17 +0300

    NotesScreen UI. Часть 2

--- app/src/main/java/com/asanagaev/note/presentation/screens/notes/NotesScreen.kt
+++ app/src/main/java/com/asanagaev/note/presentation/screens/notes/NotesScreen.kt
@@ -1,14 +1,19 @@
 package com.asanagaev.note.presentation.screens.notes
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
@@ -16,6 +21,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -28,6 +34,8 @@ import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.asanagaev.note.domain.Note
 import com.asanagaev.note.presentation.ui.theme.Green
+import com.asanagaev.note.presentation.ui.theme.OtherNotesColors
+import com.asanagaev.note.presentation.ui.theme.PinnedNotesColors
 import com.asanagaev.note.presentation.ui.theme.Yellow200
 
 @Composable
@@ -40,13 +48,19 @@ fun NotesScreen(
     LazyColumn(
         modifier = modifier
             .padding(top = 48.dp),
-        verticalArrangement = Arrangement.spacedBy(8.dp)
     ) {
         item {
-            Title(text = "All Notes")
+            Title(
+                modifier = Modifier.padding(horizontal = 24.dp),
+                text = "All Notes"
+            )
+        }
+        item {
+            Spacer(modifier = Modifier.height(16.dp))
         }
         item {
             SearchBar(
+                modifier = Modifier.padding(horizontal = 24.dp),
                 query = state.query,
                 onQueryChange = {
                     viewModel.processCommand(NotesCommand.InputSearchQuery(it))
@@ -54,18 +68,28 @@ fun NotesScreen(
             )
         }
         item {
-            Subtitle(text = "Pinned")
+            Spacer(modifier = Modifier.height(24.dp))
+        }
+        item {
+            Subtitle(
+                modifier = Modifier.padding(horizontal = 24.dp),
+                text = "Pinned"
+            )
+        }
+        item {
+            Spacer(modifier = Modifier.height(16.dp))
         }
         item {
             LazyRow(
                 modifier = Modifier
                     .fillMaxWidth(),
-                horizontalArrangement = Arrangement.spacedBy(8.dp)
+                horizontalArrangement = Arrangement.spacedBy(8.dp),
+                contentPadding = PaddingValues(horizontal = 24.dp)
             ) {
-                items(
+                itemsIndexed(
                     items = state.pinnedNotes,
-                    key = { it.id }
-                ) {note ->
+                    key = { _, note -> note.id }
+                ) { index, note ->
                     NoteCard(
                         note = note,
                         onNoteClick = {
@@ -76,21 +100,31 @@ fun NotesScreen(
                         }, onDoubleClick = {
                             viewModel.processCommand(NotesCommand.DeleteNote(note.id))
                         },
-                        backgroundColor = Yellow200
+                        backgroundColor = PinnedNotesColors[index % PinnedNotesColors.size]
                     )
                 }
             }
         }
         item {
-            Subtitle(text = "Others")
+            Spacer(modifier = Modifier.height(24.dp))
         }
-        items(
+        item {
+            Subtitle(
+                modifier = Modifier.padding(horizontal = 24.dp),
+                text = "Others"
+            )
+        }
+        item {
+            Spacer(modifier = Modifier.height(16.dp))
+        }
+        itemsIndexed(
             items = state.otherNotes,
-            key = { it.id }
-        ) {note ->
+            key = { _, note -> note.id }
+        ) { index, note ->
             NoteCard(
                 modifier = Modifier
-                    .fillMaxWidth(),
+                    .fillMaxWidth()
+                    .padding(horizontal = 24.dp),
                 note = note,
                 onNoteClick = {
                     viewModel.processCommand(NotesCommand.EditNote(note))
@@ -101,8 +135,9 @@ fun NotesScreen(
                 onDoubleClick = {
                     viewModel.processCommand(NotesCommand.DeleteNote(note.id))
                 },
-                backgroundColor = Green
+                backgroundColor = OtherNotesColors[index % OtherNotesColors.size]
             )
+            Spacer(modifier = Modifier.height(8.dp))
         }
     }
 }
@@ -129,20 +164,33 @@ private fun SearchBar(
 ) {
     TextField(
         modifier = modifier
-            .fillMaxWidth(),
+            .fillMaxWidth()
+            .background(MaterialTheme.colorScheme.surface)
+            .border(
+                width = 1.dp,
+                color = MaterialTheme.colorScheme.onSurfaceVariant,
+                shape = RoundedCornerShape(10.dp)
+            ),
         value = query,
         onValueChange = onQueryChange,
-        placeholder =  {
+        placeholder = {
             Text(
                 text = "Search...",
                 fontSize = 14.sp,
                 color = MaterialTheme.colorScheme.onSurfaceVariant,
             )
         },
+        colors = TextFieldDefaults.colors(
+            focusedContainerColor = MaterialTheme.colorScheme.surface,
+            unfocusedContainerColor = MaterialTheme.colorScheme.surface,
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent
+        ),
         leadingIcon = {
             Icon(
                 imageVector = Icons.Default.Search,
-                contentDescription = "Search Notes"
+                contentDescription = "Search Notes",
+                tint = MaterialTheme.colorScheme.onSurface
             )
         },
         shape = RoundedCornerShape(10.dp)
@@ -187,17 +235,20 @@ fun NoteCard(
                     onDoubleClick(note)
                 }
             )
+            .padding(16.dp)
     ) {
         Text(
             text = note.title,
             fontSize = 14.sp,
             color = MaterialTheme.colorScheme.onSurface
         )
+        Spacer(modifier = Modifier.height(8.dp))
         Text(
             text = note.updatedAt.toString(),
             fontSize = 12.sp,
             color = MaterialTheme.colorScheme.onSurfaceVariant
         )
+        Spacer(modifier = Modifier.height(24.dp))
         Text(
             text = note.content,
             fontSize = 16.sp,
