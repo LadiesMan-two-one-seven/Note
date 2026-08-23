7e91c59a5c6004387680d0ce23f2bbe1cceab517
LadiesMan-two-one-seven <asanagaevskiyi@list.ru>
2026-08-23 18:38:00 +0300

    Транзакции и Итоги

--- app/src/main/java/com/asanagaev/note/data/NotesDao.kt
+++ app/src/main/java/com/asanagaev/note/data/NotesDao.kt
@@ -2,28 +2,36 @@ package com.asanagaev.note.data
 
 import androidx.room.Dao
 import androidx.room.Insert
-import androidx.room.OnConflictStrategy
 import androidx.room.OnConflictStrategy.Companion.REPLACE
 import androidx.room.Query
+import androidx.room.Transaction
+import com.asanagaev.note.domain.ContentItem
+import com.asanagaev.note.domain.Note
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface NotesDao {
 
+    @Transaction
     @Query("SELECT * FROM notes ORDER BY updatedAt DESC")
     fun getAllNotes(): Flow<List<NoteWithContentDbModel>>
 
+    @Transaction
     @Query("SELECT * FROM notes WHERE id == :noteId")
     suspend fun getNote(noteId: Int): NoteWithContentDbModel
 
-    @Query("""
+    @Transaction
+    @Query(
+        """
         SELECT DISTINCT notes.* FROM notes JOIN content 
         ON notes.id == content.noteId WHERE title LIKE '%' || :query || '%' 
         OR content LIKE '%' || :query || '%' 
         ORDER BY updatedAt DESC
-        """)
+        """
+    )
     fun searchNotes(query: String): Flow<List<NoteWithContentDbModel>>
 
+    @Transaction
     @Query("DELETE FROM notes WHERE id == :noteId")
     suspend fun deleteNote(noteId: Int)
 
@@ -38,4 +46,24 @@ interface NotesDao {
 
     @Query("DELETE FROM content WHERE noteId == :noteId")
     suspend fun deleteNoteContent(noteId: Int)
+
+    @Transaction
+    suspend fun addNoteWithContent(
+        noteDbModel: NoteDbModel,
+        content: List<ContentItem>
+    ) {
+        val noteId = addNote(noteDbModel).toInt()
+        val contentItems = content.toContentItemDbModels(noteId)
+        addNoteContent(contentItems)
+    }
+
+    @Transaction
+    suspend fun updateNote(
+        noteDbModel: NoteDbModel,
+        content: List<ContentItemDbModel>
+    ) {
+        addNote(noteDbModel)
+        deleteNoteContent(noteDbModel.id)
+        addNoteContent(content)
+    }
 }
--- app/src/main/java/com/asanagaev/note/data/NotesRepositoryImpl.kt
+++ app/src/main/java/com/asanagaev/note/data/NotesRepositoryImpl.kt
@@ -20,9 +20,7 @@ class NotesRepositoryImpl @Inject constructor(
     ) {
         val processedContent = content.processForStorage()
         val noteDbModel = NoteDbModel(0, title, updatedAt, isPinned)
-        val noteId = notesDao.addNote(noteDbModel).toInt()
-        val contentItems = processedContent.toContentItemDbModels(noteId)
-        notesDao.addNoteContent(contentItems)
+        notesDao.addNoteWithContent(noteDbModel, processedContent)
     }
 
     override suspend fun deleteNote(noteId: Int) {
@@ -51,9 +49,10 @@ class NotesRepositoryImpl @Inject constructor(
         val processedContent = note.content.processForStorage()
         val processedNote = note.copy(content = processedContent)
 
-        notesDao.addNote(processedNote.toDBModel())
-        notesDao.deleteNoteContent(note.id)
-        notesDao.addNoteContent(processedContent.toContentItemDbModels(note.id))
+        notesDao.updateNote(
+            noteDbModel = processedNote.toDBModel(),
+            content = processedContent.toContentItemDbModels(note.id)
+        )
     }
 
     override suspend fun getNote(noteId: Int): Note {
